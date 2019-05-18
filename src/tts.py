db0acb2552ac49e985cd484482f3329a54b530b4
Greg Schumacher <greg.schumacher@ddb.com>
2019-05-18 16:39:55 -0700

    v2

--- src/tts.py
+++ src/tts.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/python3
+#!/usr/bin/python3
 
 import sys
 import subprocess
@@ -7,14 +7,16 @@ from sqlite3 import Error
 import random
 import math
 import can
-from pprint import pprint
+from pygame import mixer
+import time
+from var_dump import var_dump
 
 CAN_SPEAK = 0x27F
 CAN_SHUTDOWN = 0x2FF
+CAN_BUMP = 0x000
+IDGROUP_BUMP = 2
 IDGROUP_SHUTDOWN = 5
 IDGROUP_STARTUP = 6
-
-random.seed()
 
 def sqlConnection():
 	try:
@@ -24,11 +26,13 @@ def sqlConnection():
 	except Error:
 		print(Error)
 
-def getString(con, idgroup):
+def getFromGroup(con, idgroup):
+	print("Say from group {}".format(idgroup))
 	cur = con.cursor()
 	cur.execute("select count(*) from strings where idgroup = ?", (idgroup,))
 	row = cur.fetchone()
 	if row[0] == 0:
+		print("Not found")
 		return None
 	n = random.randrange(0, math.trunc(row[0] / 2 + 0.5))
 	cur.execute("select id, text, last_used from strings where idgroup = ? order by last_used limit 1 offset ?", (idgroup, n))
@@ -41,48 +45,84 @@ def getString(con, idgroup):
 	else:
 		return None
 
-def sayString(str):
+def sayString(str, vol):
 # 	print(str)
-	cmd = "pico2wave -w /tmp/pico2wave.wav \"{}\" && aplay /tmp/pico2wave.wav".format(str)
-# 	print(cmd)
+	cmd = "pico2wave -w /tmp/pico2wave.wav \"{}\"".format(str)
+	print(cmd)
 	subprocess.run([cmd], shell=True)
+	return playWav('/tmp/pico2wave.wav', vol)
 
-def sayStringFromGroup(con, idgroup):
-	str = getString(con, idgroup)
+def playWav(file, vol):
+	sound = mixer.Sound(file)
+	sound.set_volume(vol)
+	print("Play with vol {}".format(vol))
+	return sound.play() # returns channel
+
+def playSoundFromGroup(con, idgroup, vol):
+	str = getFromGroup(con, idgroup)
 	if str is not None:
-		sayString(str)
-	return str
+		if str.endswith('.wav'):
+			print("/home/pi/sounds/{}".format(str))
+			return playWav("/home/pi/sounds/{}".format(str), vol)
+		else:
+			return sayString(str, vol)
 
 def main():
+	defaultVol = 0.20
+
 	# setup
+	random.seed()
+	mixer.init(16000)
+
 	con = sqlConnection()
 
 # 	subprocess.run(["sudo ip link set can0 up type can bitrate 100000"], shell=True)
 	bus = can.interface.Bus(bustype='socketcan_native', channel='can0')
 	can_filters = [
 		{"can_id": CAN_SPEAK, "can_mask": 0x7FF, "extended": False},
-		{"can_id": CAN_SHUTDOWN, "can_mask": 0x7FF, "extended": False}
+		{"can_id": CAN_SHUTDOWN, "can_mask": 0x7FF, "extended": False},
+		{"can_id": CAN_BUMP, "can_mask": 0x7FF, "extended": False}
 	]
 	bus.set_filters(can_filters)
-	sayStringFromGroup(con, IDGROUP_STARTUP)
+	playSoundFromGroup(con, IDGROUP_STARTUP, defaultVol)
 
 	# loop
-	try:
-		while True:
-			msg = bus.recv(0.0)
-			if msg is not None:
+	while True:
+		msg = bus.recv(0.0)
+		if msg is not None:
 # 				print(msg)
-				pprint(msg)
-				if msg.arbitration_id == CAN_SPEAK and msg.dlc == 1:
-					sayStringFromGroup(con, msg.data[0])
-				elif msg.arbitration_id == CAN_SHUTDOWN:
-					sayStringFromGroup(con, IDGROUP_SHUTDOWN)
-					subprocess.run("sudo halt", shell=True)
-	except KeyboardInterrupt:
-		pass
-# 		subprocess.run(["sudo ip link set can0 down"], shell=True)
-# 		sayString("Good bye. Talk to you later.")
-
+			var_dump(msg)
+			if msg.arbitration_id == CAN_SPEAK:
+				if msg.dlc == 1:
+					playSoundFromGroup(con, msg.data[0], defaultVol)
+				elif msg.dlc > 1:
+					if msg.data[0] == 0: # set volume
+						defaultVol = msg.data[0] / 512
+						# TODO add saving defaultVol to config file and read it back in later
+					else:
+						d = 0
+						for i in range(1, msg.dlc):
+							d = d * 256 + msg.data[i]
+						if msg.data[0] == 1:
+							playSoundFromGroup(con, d, defaultVol)
+						else:
+							playSoundFromGroup(con, d, msg.data[0] / 512)
+			elif msg.arbitration_id == CAN_BUMP:
+				channel = playSoundFromGroup(con, IDGROUP_BUMP, defaultVol)
+			elif msg.arbitration_id == CAN_SHUTDOWN:
+				channel = playSoundFromGroup(con, IDGROUP_SHUTDOWN, defaultVol)
+# 				while channel.get_busy():
+# 					pass
+				time.sleep(3)
+				subprocess.run("sudo halt", shell=True)
 
 if __name__ == "__main__":
 	main()
+
+"""
+CAN data:
+DLC == 1 means play sound 0 - 255 at default volume
+data[0] == 0 set default volume = data[1]
+data[0] == 1 play sound = data[1...n]
+data[0] > 1 play sound = data[1...n] at vol = data[0]
+"""
--- src/tts_test.py
+++ src/tts_test.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/python3
+#!/usr/bin/python3
 
 import sys
 import subprocess
@@ -6,11 +6,12 @@ import sqlite3
 from sqlite3 import Error
 import random
 import math
+from pygame import mixer
+import time
+from var_dump import var_dump
 
-# if len(sys.argv) < 2:
-# 	sys.exit('Group ID required')
-
-random.seed()
+IDGROUP_SHUTDOWN = 5
+IDGROUP_STARTUP = 6
 
 def sqlConnection():
 	try:
@@ -20,73 +21,84 @@ def sqlConnection():
 	except Error:
 		print(Error)
 
-def getStrings(con, idgroup):
-	cur = con.cursor()
-	cur.execute("select id, text, last_used from strings where idgroup = ? order by last_used", (idgroup,))
-	rows = cur.fetchall()
-	for row in rows:
-		print(row)
-	print(cur.rowcount)
-
-# def getString(con, idgroup):
-# 	cur = con.cursor()
-# 	cur.execute("select count(*) from strings where idgroup = ?", (idgroup,))
-# 	row = cur.fetchone()
-# 	n = random.randrange(0, math.trunc(row[0] / 2 + 0.5))
-# 	cur.execute("select id, text, last_used from strings where idgroup = ? order by last_used", (idgroup,))
-# 	row = None
-# 	while n >= 0:
-# 		row = cur.fetchone()
-# 		n -= 1
-# 	print(row)
-# 	if row:
-# 		print(row['id'])
-# 		cur.execute("update strings set last_used = strftime('\%s', 'now') where id = ?", (row['id'],))
-# 		con.commit()
-# 		return row['text']
-# 	else:
-# 		return ""
-
-def getString(con, idgroup):
+def getFromGroup(con, idgroup):
+	print("Say from group {}".format(idgroup))
 	cur = con.cursor()
 	cur.execute("select count(*) from strings where idgroup = ?", (idgroup,))
 	row = cur.fetchone()
 	if row[0] == 0:
-		return "Speech group not found."
+		print("Not found")
+		return None
 	n = random.randrange(0, math.trunc(row[0] / 2 + 0.5))
 	cur.execute("select id, text, last_used from strings where idgroup = ? order by last_used limit 1 offset ?", (idgroup, n))
 	row = cur.fetchone()
 	if row:
-# 		print(row['id'])
-		cur.execute("update strings set last_used = strftime('\%s', 'now') where id = ?", (row['id'],))
+#		print(row['id'])
+		cur.execute("update strings set last_used = strftime('%s', 'now') where id = ?", (row['id'],))
 		con.commit()
 		return row['text']
 	else:
-		return ""
+		return None
 
-def saySomething(str):
-	print(str)
-	cmd = "pico2wave -w /tmp/pico2wave.wav \"{}\" && aplay /tmp/pico2wave.wav".format(str)
+def sayString(str, vol):
+# 	print(str)
+	cmd = "pico2wave -w /tmp/pico2wave.wav \"{}\"".format(str)
+# 	cmd = "pico2wave -w /tmp/pico2wave.wav \"{}\" && aplay /tmp/pico2wave.wav".format(str)
 	print(cmd)
 	subprocess.run([cmd], shell=True)
+	return playWav('/tmp/pico2wave.wav', vol)
+
+def playWav(file, vol):
+	sound = mixer.Sound(file)
+	sound.set_volume(vol)
+	print("Play with vol {}".format(vol))
+	return sound.play() # returns channel
+
+def playSoundFromGroup(con, idgroup, vol):
+	str = getFromGroup(con, idgroup)
+	if str is not None:
+		if str.endswith('.wav'):
+			print("/home/pi/sounds/{}".format(str))
+			return playWav("/home/pi/sounds/{}".format(str), vol)
+		else:
+			return sayString(str, vol)
 
 def main():
+	defaultVol = 0.20
+
+	# setup
+	random.seed()
+	mixer.init(16000)
+
 	con = sqlConnection()
-	saySomething("gidget is ready to go")
+
+	playSoundFromGroup(con, IDGROUP_STARTUP, defaultVol)
+
+	# loop
 	while True:
 		try:
 			n = input('Input:')
 			if n == 'q' or n == 'Q':
-				saySomething("good bye")
+				channel = playSoundFromGroup(con, IDGROUP_SHUTDOWN, defaultVol)
+# 				while channel.get_busy():
+# 					pass
+				time.sleep(3)
 				sys.exit()
-			n = int(n)
-			print(n)
-			if n:
-				str = getString(con, n)
-				saySomething(str)
-				n = 0
+			if n.startswith('v') or n.startswith('V'):
+				n = int(n[1:])
+# 				print(n)
+				if n >= 0 and n <= 255:
+					defaultVol = int(n / 512 * 100) / 100
+					var_dump(defaultVol)
+					sayString("Volume set to {}.".format(defaultVol), defaultVol)
+			else:
+				n = int(n)
+# 				print(n)
+				if n:
+					playSoundFromGroup(con, n, defaultVol)
 		except ValueError:
 			print("Not a number")
 
 if __name__ == "__main__":
-    main()
+	main()
+
